a50f02bb8a90f9347b830987b1213418084c614e
kupuma-ru21 <tech.kupumaru@gmail.com>
2025-03-03 13:36:49 -0800

    chore: wip

--- db.zsh
+++ db.zsh
@@ -2,14 +2,14 @@
 
 default_branch=$(git get-default-branch)
 protected_branches=$(git get-pushed-branches-with-diff-remote | tr '\n' '|')
-protected_branches=$(git branch --format \"%(refname:short)\" --no-merged origin/$default_branch | tr \"\\n\" \"|\")
-
-echo "protected_branches: $protected_branches"
-
-
-echo "protected_branches: $protected_branches"
-
 protected_branches=${protected_branches%|}
+
+git for-each-ref --format="%(refname:short)" refs/heads | while read branch; do
+  if ! git ls-remote --heads origin "$branch" | grep -q "$branch"; then
+    echo "$branch"
+  fi
+done
+
 
 if [[ -z "$protected_branches" ]]; then
   git branch | grep -v "^\*" | xargs git branch -D
